47d3cd12350f1546482643e720dbed8b4e03d523
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-07-09 15:47:56 -0300

    stage correction

--- build/api/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = require("./routes");
-const PORT = process.env.PORT || 3000;
-routes_1.app.listen(PORT, () => console.log('Listen on port', PORT));
